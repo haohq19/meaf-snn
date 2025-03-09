7bb4a4bd93fb20867213e93ac7edfcbb5b1bca57
haohq19 <haohq19@gmail.com>
2025-03-09 13:36:44 +0800

    cache logits/preds/features

---
+++ cache_static.py
@@ -0,0 +1,134 @@
+# Cache pseudo labels / pseudo logits / feature maps for transferability assessment
+
+import os
+import argparse
+import torch
+import torch.nn as nn
+import numpy as np
+import tqdm
+from torch.utils.data import DataLoader
+import models.spiking_resnet_static as spiking_resnet
+import models.sew_resnet_static as sew_resnet
+from spikingjelly.activation_based import functional
+from utils.data import get_static_data_loader
+
+
+def parser_args():
+    parser = argparse.ArgumentParser()
+    # data
+    parser.add_argument('--dataset', default='cifar10', type=str, help='dataset')
+    parser.add_argument('--batch_size', default=128, type=int, help='batch size')
+    parser.add_argument('--num_classes', default=10, type=int, help='number of classes')
+    parser.add_argument('--nsteps', default=4, type=int, help='number of time steps')
+    parser.add_argument('--connect_f', default='ADD', type=str, help='spike-element-wise connect function')
+    parser.add_argument('--device_id', default=0, type=int, help='GPU id to use.')
+    parser.add_argument('--nworkers', default=8, type=int, help='number of workers')
+    parser.add_argument('--pt_dir', default='weights/static', help='path to pretrained weights')
+    parser.add_argument('--output_dir', default='outputs', help='path where to save')
+    return parser.parse_args()
+
+
+def cache_representations(
+    model: nn.Module,
+    data_loader: DataLoader,
+    cache_dir: str,
+):  
+    if os.path.exists(cache_dir):
+        print('Cache already exists')
+        return
+    else:
+        os.makedirs(cache_dir)
+        print('Cache not found, make cache directory at [{}]'.format(cache_dir))
+    
+    with torch.no_grad():
+        model.eval()
+        features = []
+        logits = []
+        labels = []
+        nsteps_per_epoch = len(data_loader)
+        process_bar = tqdm.tqdm(total=nsteps_per_epoch)
+        for data, label in data_loader:
+            if len(data.shape) == 4:  # check static data shape: (B, C, H, W)
+                input = data.cuda(non_blocking=True)
+            else:
+                raise ValueError('Invalid data shape')
+
+            label = label.numpy()                                   # label.shape = (B,)
+            output = model(input)                                   # output.shape = (B, num_classes)
+            
+            feature_map = model.feature.detach().cpu().numpy()      # feature_map.shape = (T, B, num_features)
+            features.append(feature_map)
+            logit = output.softmax(dim=1).detach().cpu().numpy()    # logit.shape = (B, num_classes)
+            logits.append(logit)
+            labels.append(label)
+
+            functional.reset_net(model)
+            process_bar.update(1)
+        process_bar.close()
+        
+        features = np.concatenate(features, axis=1).transpose(1, 0, 2)  # features.shape = (T, nsamples, num_features) -> (nsamples, T, num_features)
+        logits = np.concatenate(logits, axis=0)                         # logits.shape = (nsamples, num_classes)
+        labels = np.concatenate(labels, axis=0)                         # labels.shape = （nsamples,）
+        np.save(os.path.join(cache_dir, 'features.npy'), features)
+        np.save(os.path.join(cache_dir, 'logits.npy'), logits)
+        np.save(os.path.join(cache_dir, 'labels.npy'), labels)
+
+
+
+def load_pretrained_model(args):
+    pt_path = os.path.join(args.pt_dir, args.model + '.pth')
+    if not os.path.exists(pt_path):
+        raise FileNotFoundError(pt_path)
+    else:
+        checkpoint = torch.load(pt_path)
+        if 'model' in checkpoint.keys():
+            pt_weights = checkpoint['model']
+        else:
+            pt_weights = checkpoint
+
+    # model
+    if args.model in sew_resnet.__dict__:
+        model = sew_resnet.__dict__[args.model](num_classes=1000, T=args.nsteps, connect_f=args.connect_f)
+        if pt_weights is not None:
+            state_dict = {k.replace('module.', ''): v for k, v in pt_weights.items()}
+            model.load_state_dict(state_dict)
+            print('Load pretrained weights from [{}]'.format(pt_path))
+    
+    elif args.model in spiking_resnet.__dict__:
+        model = spiking_resnet.__dict__[args.model](num_classes=1000, T=args.nsteps)
+        if pt_weights is not None:
+            state_dict = {k.replace('module.', ''): v for k, v in pt_weights.items()}
+            model.load_state_dict(state_dict)
+            print('Load pretrained weights from [{}]'.format(pt_path))
+
+    else:
+        raise NotImplementedError(args.model)
+    return model
+
+
+def main(args, data_loader, dataset_type):
+    # device
+    torch.cuda.set_device(args.device_id)
+
+    # model
+    model = load_pretrained_model(args)
+    model.cuda()
+    
+    # cache representations
+    cache_dir = os.path.join(args.output_dir, 'cache', args.dataset, args.model, dataset_type)  # output_dir/dataset/model/cache
+    if not os.path.exists(cache_dir):
+        cache_representations(
+            model=model,
+            data_loader=data_loader,
+            cache_dir=cache_dir,
+            dataset_type=dataset_type,
+        )
+
+
+if __name__ == '__main__':
+    models = ['sew_resnet18', 'sew_resnet34', 'sew_resnet50', 'sew_resnet101', 'sew_resnet152', 'spiking_resnet18', 'spiking_resnet34', 'spiking_resnet50']
+    args = parser_args()
+    train_loader, _, _ = get_static_data_loader(args)
+    for model in models:
+        args.model = model
+        main(args, train_loader, 'train')
---
+++ utils/data.py
@@ -0,0 +1,165 @@
+import math
+import torch
+import numpy as np
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import CIFAR10, CIFAR100, Caltech101, Caltech256, DTD, Food101, MNIST
+from spikingjelly.datasets import cifar10_dvs, dvs128_gesture, n_caltech101, n_mnist
+
+
+def split2dataset(train_ratio: float, origin_dataset: torch.utils.data.Dataset, num_classes: int, random_split: bool = True):
+    '''
+    :param train_ratio: split the ratio of the origin dataset as the train set
+    :type train_ratio: float
+    :param origin_dataset: the origin dataset
+    :type origin_dataset: torch.utils.data.Dataset
+    :param num_classes: total classes number, e.g., ``10`` for the MNIST dataset
+    :type num_classes: int
+    :param random_split: If ``False``, the front ratio of samples in each classes will
+            be included in train set, while the reset will be included in test set.
+            If ``True``, this function will split samples in each classes randomly. The randomness is controlled by
+            ``numpy.randon.seed``
+    :type random_split: int
+    :return: a tuple ``(train_set, test_set)``
+    :rtype: tuple
+    '''
+    label_idx = []
+    for i in range(num_classes):
+        label_idx.append([])
+
+    for i, item in enumerate(origin_dataset):
+        y = item[1]
+        if isinstance(y, np.ndarray) or isinstance(y, torch.Tensor):
+            y = y.item()
+        label_idx[y].append(i)
+    train_idx = []
+    test_idx = []
+    if random_split:
+        for i in range(num_classes):
+            np.random.shuffle(label_idx[i])
+
+    for i in range(num_classes):
+        pos = math.ceil(label_idx[i].__len__() * train_ratio)
+        train_idx.extend(label_idx[i][0: pos])
+        test_idx.extend(label_idx[i][pos: label_idx[i].__len__()])
+
+    return torch.utils.data.Subset(origin_dataset, train_idx), torch.utils.data.Subset(origin_dataset, test_idx)
+
+
+def split3dataset(train_ratio: float, val_ratio: float, origin_dataset: torch.utils.data.Dataset, num_classes: int, random_split: bool = True):
+    '''
+    :param train_ratio: split the ratio of the origin dataset as the train set
+    :param val_ratio: split the ratio of the origin dataset as the validation set
+    :param origin_dataset: the origin dataset
+    :param num_classes: total classes number
+    :param random_split: If ``False``, the front ratio of samples in each classes will
+            be included in train set, while the reset will be included in test set.
+            If ``True``, this function will split samples in each classes randomly. The randomness is controlled by
+            ``numpy.randon.seed``
+    :return: a tuple ``(train_set, val_set, test_set)``
+    '''
+    label_idx = []
+    for i in range(num_classes):
+        label_idx.append([])
+
+    for i, item in enumerate(origin_dataset): 
+        y = item[1]  # item[1] is the label
+        if isinstance(y, np.ndarray) or isinstance(y, torch.Tensor):
+            y = y.item()  # convert to int
+        label_idx[y].append(i)
+    train_idx = []
+    val_idx = []
+    test_idx = []
+    if random_split:
+        for i in range(num_classes):
+            np.random.shuffle(label_idx[i])
+
+    for i in range(num_classes):
+        pos_train = math.ceil(label_idx[i].__len__() * train_ratio)
+        pos_val = math.ceil(label_idx[i].__len__() * (train_ratio + val_ratio))
+        train_idx.extend(label_idx[i][0: pos_train])
+        val_idx.extend(label_idx[i][pos_train: pos_val])
+        test_idx.extend(label_idx[i][pos_val: label_idx[i].__len__()])
+
+    return torch.utils.data.Subset(origin_dataset, train_idx), torch.utils.data.Subset(origin_dataset, val_idx), torch.utils.data.Subset(origin_dataset, test_idx)
+
+
+def get_event_data_loader(args):
+
+    if args.dataset == 'cifar10_dvs':
+        dataset = cifar10_dvs.CIFAR10DVS(root='/home/haohq/datasets/CIFAR10DVS', data_type='frame', frames_number=args.nsteps, split_by='time')
+        train_dataset, val_dataset, test_dataset = split3dataset(0.8, 0.1, dataset, args.num_classes, random_split=False)
+
+    elif args.dataset == 'dvs128_gesture':  
+        dataset = dvs128_gesture.DVS128Gesture
+        train_dataset = dataset(root='/home/haohq/datasets/DVS128Gesture', train=True, data_type='frame', frames_number=args.nsteps, split_by='time')
+        test_dataset = dataset(root='/home/haohq/datasets/DVS128Gesture', train=False, data_type='frame', frames_number=args.nsteps, split_by='time')
+        train_dataset, val_dataset = split2dataset(0.8, train_dataset, args.num_classes, random_split=False)
+
+    elif args.dataset == 'n_caltech101':
+        dataset = n_caltech101.NCaltech101(root='/home/haohq/datasets/NCaltech101', data_type='frame', frames_number=args.nsteps, split_by='time')
+        train_dataset, val_dataset, test_dataset= split3dataset(0.8, 0.1, dataset, args.num_classes, random_split=False)
+
+    elif args.dataset == 'n_mnist':
+        train_dataset = n_mnist.NMNIST(root='/home/haohq/datasets/NMNIST', train=True, data_type='frame', frames_number=args.nsteps, split_by='time')
+        test_dataset = n_mnist.NMNIST(root='/home/haohq/datasets/NMNIST', train=False, data_type='frame', frames_number=args.nsteps, split_by='time')
+        train_dataset, val_dataset = split2dataset(0.9, train_dataset, args.num_classes, random_split=False)
+
+    else:
+        raise NotImplementedError(args.dataset)
+    
+    train_sampler = torch.utils.data.RandomSampler(train_dataset)
+    val_sampler = torch.utils.data.SequentialSampler(val_dataset)
+    test_sampler = torch.utils.data.SequentialSampler(test_dataset)
+
+    return DataLoader(train_dataset, batch_size=32, sampler=train_sampler, num_workers=args.nworkers, pin_memory=True, drop_last=False), \
+        DataLoader(val_dataset, batch_size=32, sampler=val_sampler, num_workers=args.nworkers, pin_memory=True, drop_last=False), \
+        DataLoader(test_dataset, batch_size=32, sampler=test_sampler, num_workers=args.nworkers, pin_memory=True, drop_last=False)
+
+
+
+class EnsureRGB:
+    def __call__(self, img):
+        if img.mode == 'L':
+            return img.convert('RGB')
+        return img
+
+
+def get_static_data_loader(args):
+
+    _transform = transforms.Compose([
+    EnsureRGB(),
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406], 
+        std=[0.229, 0.224, 0.225]
+    )
+])
+    
+    if args.dataset == 'cifar10':
+        train_dataset = CIFAR10(train=True, root='/home/haohq/datasets/CIFAR10', download=True, transform=_transform)
+        test_dataset = CIFAR10(train=False, root='/home/haohq/datasets/CIFAR10', download=True, transform=_transform)
+        train_dataset, val_dataset = split2dataset(0.9, train_dataset, num_classes=args.num_classes, random_split=False)
+    elif args.dataset == 'cifar100':
+        train_dataset = CIFAR100(train=True, root='/home/haohq/datasets/CIFAR100', download=True, transform=_transform)
+        test_dataset = CIFAR100(train=False, root='/home/haohq/datasets/CIFAR100', download=True, transform=_transform)
+        train_dataset, val_dataset = split2dataset(0.9, train_dataset, num_classes=args.num_classes, random_split=False)
+    elif args.dataset == 'caltech101':
+        dataset = Caltech101(root='/home/haohq/datasets/Caltech101', download=True, transform=_transform)
+        train_dataset, val_dataset, test_dataset = split3dataset(0.8, 0.1, dataset, num_classes=args.num_classes, random_split=False)
+    elif args.dataset == 'mnist':
+        train_dataset = MNIST(root='/home/haohq/datasets/MNIST', train=True, transform=_transform, download=True)
+        test_dataset = MNIST(root='/home/haohq/datasets/MNIST', train=False, transform=_transform, download=True)
+        train_dataset, val_dataset = split2dataset(0.9, train_dataset, num_classes=args.num_classes, random_split=False)
+    else:
+        raise NotImplementedError(args.dataset)
+    
+    train_sampler = torch.utils.data.RandomSampler(train_dataset)
+    val_sampler = torch.utils.data.SequentialSampler(val_dataset)
+    test_sampler = torch.utils.data.SequentialSampler(test_dataset)
+
+    return DataLoader(train_dataset, batch_size=args.batch_size, sampler=train_sampler, num_workers=args.nworkers, pin_memory=True, drop_last=False), \
+        DataLoader(val_dataset, batch_size=args.batch_size, sampler=val_sampler, num_workers=args.nworkers, pin_memory=True, drop_last=False), \
+        DataLoader(test_dataset, batch_size=args.batch_size, sampler=test_sampler, num_workers=args.nworkers, pin_memory=True, drop_last=False)
